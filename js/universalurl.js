0d2d05c8e3844e8570f3eb1d610e76ebe1aef899
Syedameerjan <133519135+Syedameerjan@users.noreply.github.com>
2024-10-04 18:45:37 +0530

    Add files via upload

---
+++ js/universalurl.js
@@ -0,0 +1,101 @@
+
+// ***Gallery URL Start***
+
+const gTeachersTrining = "https://photos.app.goo.gl/LUtGXtE6NxmDsX5dA";
+// Apply the URL to all anchor tags with a specific class
+document.querySelectorAll('.Teachers-Trining').forEach(link => {
+    link.href = gTeachersTrining;
+});
+
+
+const gLeadersTrining = "https://photos.app.goo.gl/zZqC3NmVT73pvRHL8";
+document.querySelectorAll('.Leaders-Trining').forEach(link => {
+    link.href = gLeadersTrining;
+});
+
+const gBusinessTrining = "https://photos.app.goo.gl/L8XG5gNPZzh6jUs98";
+document.querySelectorAll('.Business-Trining').forEach(link => {
+    link.href = gBusinessTrining;
+});
+
+const gCommunicativeEnglish = "https://photos.app.goo.gl/Gnenh2oNS98gnepr5";
+document.querySelectorAll('.Communicative-English').forEach(link => {
+    link.href = gCommunicativeEnglish;
+});
+
+const gBibleTraining = "https://photos.app.goo.gl/h5BG5EBZredFvvZ66";
+document.querySelectorAll('.Bible-Training').forEach(link => {
+    link.href = gBibleTraining;
+});
+
+const gIeltsTraining = "https://photos.app.goo.gl/Lhea18QCsV4zhXUTA";
+document.querySelectorAll('.Ielts-Training').forEach(link => {
+    link.href = gIeltsTraining;
+});
+
+// ***Gallery URL End***
+
+// ***Map URL Start***
+
+const mapUrl = "https://maps.app.goo.gl/JVX8cftCnsdEPSDD7";
+
+document.querySelectorAll('.Map-Url').forEach(link => {
+    link.href = mapUrl;
+});
+
+// ***Map URL End***
+
+
+// ***Share URL Start***
+
+const watasUrl = "https://wa.me/917560910133";
+document.querySelectorAll('.Wats-Url').forEach(link => {
+    link.href = watasUrl;
+});
+
+
+const fbUrl = "https://www.facebook.com/profile.php?id=100087985568973";
+document.querySelectorAll('.FB-Url').forEach(link => {
+    link.href = fbUrl;
+});
+
+
+const instaUrl = "https://www.instagram.com/hannah_jones_academy";
+document.querySelectorAll('.Insta-Url').forEach(link => {
+    link.href = instaUrl;
+});
+
+
+const threadUrl = "https://wa.me/qr/DXZ2S74EMTTZD1";
+document.querySelectorAll('.Thread-Url').forEach(link => {
+    link.href = threadUrl;
+});
+
+// ***Share URL End***
+
+
+// ***Form URL Start***
+
+const formIELTSUrl = "https://surveyheart.com/form/66f6f38c81d71a69024ebf6f";
+document.querySelectorAll('.Form-IELTS-Url').forEach(link => {
+    link.href = formIELTSUrl;
+});
+
+const formCommunicativeEnglishUrl = "https://surveyheart.com/form/66f6f38c81d71a69024ebf6f";
+document.querySelectorAll('.Form-CE-Url').forEach(link => {
+    link.href = formCommunicativeEnglishUrl;
+});
+
+const formBusinessUrl = "https://surveyheart.com/form/66f6f38c81d71a69024ebf6f";
+document.querySelectorAll('.Form-Business-Url').forEach(link => {
+    link.href = formBusinessUrl;
+});
+
+const formBibleUrl = "https://surveyheart.com/form/66f6f38c81d71a69024ebf6f";
+document.querySelectorAll('.Form-Bible-Url').forEach(link => {
+    link.href = formBibleUrl;
+});
+
+
+
+
